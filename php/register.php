2c7ba0a597f16401e9c870bc55e4097e2fbe200b
kshitiz-patel <87030246+kshitiz-patel@users.noreply.github.com>
2022-02-10 22:07:36 +0530

    Add files via upload

---
+++ php/register.php
@@ -0,0 +1,154 @@
+<?php
+session_start();
+require_once "./config.php";
+
+if (isset($_POST) & !empty($_POST)) {
+    $name = mysqli_real_escape_string($connection, $_POST["name"]); // 'x y z'
+  $email = mysqli_real_escape_string($connection, $_POST["email"]);
+  $password = md5($_POST["password"]);
+  $sql = "INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES (NULL, '$name', '$email', '$password');";
+  
+  $res = mysqli_query($connection, $sql);
+  if ($res) {
+    $_SESSION["email"] = $email;
+    header("location: dashboard.php");
+  } else {
+    $fmsg = "Failed to Submit";
+  }
+  
+  // dhgaduadhasdah = test;
+
+  // md5(test) = dhgaduadhasdah;
+  
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/main.css" />
+    <title>Prime Video Sign-up</title>
+  </head>
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    input[type=text], input[type=email],input[type=password], select, textarea {
+      width: 100%;
+      padding: 12px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      resize: vertical;
+    }
+
+    label {
+      padding: 12px 12px 12px 0;
+      display: inline-block;
+    }
+
+    input[type=submit] {
+      background-color: #04AA6D;
+      color: white;
+      padding: 12px 20px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+      float: right;
+    }
+
+    input[type=submit]:hover {
+      background-color: #45a049;
+    }
+
+    .container {
+      border-radius: 5px;
+      background-color: rgba(0,0,0, 0.8);
+      padding: 20px;
+      min-width:50vw;
+    }
+
+    .col-25 {
+      float: left;
+      width: 25%;
+      margin-top: 6px;
+    }
+
+    .col-75 {
+      float: left;
+      width: 75%;
+      margin-top: 6px;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+
+    /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
+      .col-25, .col-75, input[type=submit] {
+        width: 100%;
+        margin-top: 0;
+      }
+      .container{
+        margin: auto;
+        min-width: 80vw;
+        
+      }
+    }
+</style>
+  <body>
+    <header class="showcase">
+      <div class="showcase-top">
+        <img src="../primelogo.png" alt="" />
+        <a href="./login.php" class="text-md">Login</a>
+      </div>
+      <div class="showcase-content">
+        <div class="container">
+        <center style="">
+            <h2>Registeration</h2>
+        </center>
+          <form method="POST">
+            <div class="row">
+              <div class="col-25">
+                <label for="name">Name</label>
+              </div>
+              <div class="col-75">
+                <input type="text" id="fname" name="name" placeholder="Your name.." required>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="name">Email</label>
+              </div>
+              <div class="col-75">
+                <input type="email" id="fname" name="email" placeholder="Email..." required>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-25">
+                <label for="name">Password</label>
+              </div>
+              <div class="col-75">
+                <input type="password" id="fname" name="password" placeholder="Password....." required>
+              </div>
+            </div>
+
+            <br>
+              <input type="submit" value="Submit">
+            
+            
+            
+          </form>
+        </div>
+      </div>
+    </header> 
+  </body>
+</html>
+
+
